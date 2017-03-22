e34c790e33d194d1f349c8e6b46d5aaf74879007
Servius <nosamrj@gmail.com>
2017-03-22 16:18:37 -0700

    Initial commit of Tedukens work

---
+++ tfa_swrp_imperial_era_weapons_pack/lua/autorun/server/init.lua
@@ -0,0 +1,9 @@
+local function Plug( )
+
+print("The TFA IMPERIAL ERA SWEP PACK was made by Teduken")
+print("This version is the github development branch edited by Servius.")
+print("Temporarily removed smartrocket")
+print("Changed T-21B from an RT scope to a scope overlay")
+print("Fixed tracers, if you already had a star wars weapons pack installed besides this one you wouldn't have noticed this issue")
+end
+hook.Add("PlayerInitialSpawn", "Updates", Plug)
