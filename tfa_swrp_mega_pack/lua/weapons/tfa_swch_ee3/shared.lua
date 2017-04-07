2c5f276dcba3f17d459311008a84bf37ab9b9b9c
Captain Church <church@lbgaming.net>
2017-04-07 05:08:33 -0400

    Fixed function of EE-3

--- tfa_swrp_mega_pack/lua/weapons/tfa_swch_ee3/shared.lua
+++ tfa_swrp_mega_pack/lua/weapons/tfa_swch_ee3/shared.lua
@@ -8,14 +8,14 @@ end
 
 if ( CLIENT ) then
 
-	SWEP.PrintName			= "DC-15S"			
+	SWEP.PrintName			= "EE-3"			
 	SWEP.Author				= "Syntax_Error752"
 	SWEP.ViewModelFOV      	= 50
 	SWEP.Slot				= 2
 	SWEP.SlotPos			= 3
-	SWEP.WepSelectIcon = surface.GetTextureID("HUD/killicons/DC15S")
+	SWEP.WepSelectIcon 		= surface.GetTextureID("HUD/killicons/EE3")
 	
-	killicon.Add( "weapon_752_dc15s", "HUD/killicons/DC15S", Color( 255, 80, 0, 255 ) )
+	killicon.Add( "weapon_752_ee3", "HUD/killicons/EE3", Color( 255, 80, 0, 255 ) )
 	
 end
 
@@ -27,43 +27,134 @@ SWEP.Category = "TFA Star Wars"
 SWEP.Spawnable				= true
 SWEP.AdminSpawnable			= true
 
-SWEP.ViewModel				= "models/weapons/v_DC15S.mdl"
-SWEP.WorldModel				= "models/weapons/w_DC15S.mdl"
+SWEP.ViewModel				= "models/weapons/v_EE3.mdl"
+SWEP.WorldModel				= "models/weapons/w_EE3.mdl"
 
 SWEP.Weight					= 5
 SWEP.AutoSwitchTo			= false
 SWEP.AutoSwitchFrom			= false
 
-SWEP.Primary.Sound = Sound ("weapons/DC15S_fire.wav");
-SWEP.Primary.ReloadSound = Sound ("weapons/DC15S_reload.wav");
+SWEP.Primary.Sound = Sound ("weapons/EE3_fire.wav");
+SWEP.Primary.ReloadSound = Sound ("weapons/EE3_reload.wav");
 
 SWEP.Primary.Recoil			= 0.5
-SWEP.Primary.Damage			= 50
+SWEP.Primary.Damage			= 70
 SWEP.Primary.NumShots		= 1
-SWEP.Primary.Spread			= 0.0125
+SWEP.Primary.Spread		    = 0.0125				--This is hip-fire acuracy.  Less is more (1 is horribly awful, .0001 is close to perfect)
+SWEP.Primary.IronAccuracy   = 0.0125
 SWEP.Primary.ClipSize		= 50
-SWEP.Primary.RPM = 60/0.25
 SWEP.Primary.DefaultClip	= 150
-SWEP.Primary.Automatic		= true
+SWEP.Primary.Automatic = false -- Automatic/Semi Auto
+SWEP.Primary.RPM = 600 -- This is in Rounds Per Minute / RPM
+SWEP.Primary.RPM_Burst = 600 -- RPM for burst fire, overrides semi.  This is in Rounds Per Minute / RPM
+SWEP.Primary.BurstDelay = nil -- Delay between bursts, leave nil to autocalculate
 SWEP.Primary.Ammo			= "ar2"
-SWEP.TracerName = "effect_sw_laser_blue"
+SWEP.TracerName = "effect_sw_laser_red"
 
 SWEP.Secondary.Automatic	= false
 SWEP.Secondary.Ammo			= "none"
 
-SWEP.IronSightsPos = Vector(-4.06, -9.219, 1.395)
-SWEP.IronSightsAng = Vector(-0.454, 0.7, 0.5)
-
-SWEP.ShowWorldModel = false
-
-SWEP.ViewModelBoneMods = {
-	["v_thompson"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+--Range Related
+SWEP.Primary.Range = 500 -- The distance the bullet can travel in source units.  Set to -1 to autodetect based on damage/rpm.
+SWEP.Primary.RangeFalloff = 0.25 -- The percentage of the range the bullet damage starts to fall off at.  Set to 0.8, for example, to start falling off after 80% of the range.
+-- Selective Fire Stuff
+ SWEP.FireModes = {
+"3Burst",
+"Single"
 }
+
+SWEP.SelectiveFire = true --Allow selecting your firemode?
+SWEP.OnlyBurstFire = true --No auto, only burst/single?
+SWEP.DefaultFireMode = "3Burst" --Default to auto or whatev
+
+
+SWEP.IronSightsPos = Vector(-4.64, -12.938, 1.059)
+SWEP.IronSightsAng = Vector(0, 0, -0.491)
 
 SWEP.VElements = {
-	["element_name"] = { type = "Model", model = "models/swbf3/rep/blaster.mdl", bone = "v_thompson.root5", rel = "", pos = Vector(1.904, 5.247, -0.265), angle = Angle(0.699, 2.354, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["element_name"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_thompson.root5", rel = "", pos = Vector(-2.362, -2.445, 0.17), angle = Angle(0, 180, 0), size = Vector(0.259, 0.259, 0.259), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} }
 }
 
-SWEP.WElements = {
-	["element_name"] = { type = "Model", model = "models/swbf3/rep/blaster.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(10.491, 0.898, 0.773), angle = Angle(-11.723, 0, 180), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
-}
+local function  drawFilledCircle( x, y, radius, seg )
+	local kirkle = {}
+
+	table.insert(kirkle, { x = x, y = y, u = 0.5, v = 0.5 } )
+	for i = 0, seg do
+		local a = math.rad( ( i / seg ) * -360 )
+		table.insert(kirkle, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+	end
+
+	local a = math.rad( 0 ) -- This is need for non absolute segment counts
+	table.insert(kirkle, { x = x + math.sin( a ) * radius, y = y + math.cos( a ) * radius, u = math.sin( a ) / 2 + 0.5, v = math.cos( a ) / 2 + 0.5 } )
+
+	surface.DrawPoly(kirkle)
+end
+	
+local weaponcol = Color(0.435*255,0.10*255,0.7*255,255)
+
+local ceedee = {}
+
+SWEP.RTMaterialOverride = -1 --the number of the texture, which you subtract from GetAttachment
+
+SWEP.RTOpaque = true
+
+local g36
+if surface then
+	g36 = surface.GetTextureID("scope/gdcw_nvgilluminatedduplex") --the texture you vant to use
+end
+
+SWEP.RTCode = function( self, mat )
+	
+	render.OverrideAlphaWriteEnable( true, true)
+	surface.SetDrawColor(color_white)
+	surface.DrawRect(-256,-256,512,512)
+	render.OverrideAlphaWriteEnable( true, true)
+	
+	local ang = self.Owner:EyeAngles()
+	
+	local AngPos = self.Owner:GetViewModel():GetAttachment(1)
+	
+	if AngPos then
+	
+		ang = AngPos.Ang
+		
+		ang:RotateAroundAxis(ang:Right(), -0.02)
+		ang:RotateAroundAxis(ang:Up(), 0.07)
+
+	end
+	
+
+	
+	ceedee.angles = ang
+	ceedee.origin = self.Owner:GetShootPos()
+	
+	ceedee.x = 0
+	ceedee.y = 0
+	ceedee.w = 512	
+	ceedee.h = 512
+	ceedee.fov = 6
+	ceedee.drawviewmodel = false
+	ceedee.drawhud = false
+	
+	
+	if self.CLIronSightsProgress>0.01 then
+		render.RenderView(ceedee)
+	end
+		
+	render.OverrideAlphaWriteEnable( false, true)
+	
+	
+	cam.Start2D()
+		draw.NoTexture()
+		surface.SetDrawColor(ColorAlpha(color_black,0))
+		surface.DrawTexturedRect(0,0,512,512)
+		surface.SetDrawColor(color_white)
+		surface.SetTexture(g36)
+		surface.DrawTexturedRect(-256,-256,1024,1024)
+		draw.NoTexture()
+		surface.SetDrawColor(ColorAlpha(color_black,(1-self.CLIronSightsProgress)*255))
+		surface.DrawTexturedRect(0,0,512,512)
+	cam.End2D()
+	
+end
+SWEP.IronSightsSensitivity = 0.25 --Useful for a RT scope.  Change this to 0.25 for 25% sensitivity.  This is if normal FOV compenstaion isn't your thing for whatever reason, so don't change it for normal scopes.
