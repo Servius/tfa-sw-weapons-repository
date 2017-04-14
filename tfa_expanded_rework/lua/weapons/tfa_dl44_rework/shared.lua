69d9019ed674dd304ba704c656d90e59f00ba5a4
Servius <nosamrj@gmail.com>
2017-04-14 02:05:59 -0700

    Rework Folder added for all scoped sweps

--- tfa_expanded_rework/lua/weapons/tfa_dl44_rework/shared.lua
+++ tfa_expanded_rework/lua/weapons/tfa_dl44_rework/shared.lua
@@ -2,11 +2,11 @@ if ( SERVER ) then
 	AddCSLuaFile( "shared.lua" )
 end
 if ( CLIENT ) then
-	SWEP.PrintName			= "DH-17"	
-	SWEP.Author				= "TFA, Servius"
-	SWEP.ViewModelFOV      	= 50
-	SWEP.Slot				= 2
-	SWEP.SlotPos			= 3
+	SWEP.PrintName = "Dl-44"	
+	SWEP.Author = "TFA, Servius"
+	SWEP.ViewModelFOV = 50
+	SWEP.Slot = 2
+	SWEP.SlotPos = 3
 end
 SWEP.Base = "tfa_3dscoped_base"
 SWEP.Category = "TFA Star Wars"
@@ -20,18 +20,18 @@ SWEP.WorldModel = "models/weapons/w_dl44.mdl"
 SWEP.ShowViewModel = true
 SWEP.ShowWorldModel = false
 SWEP.UseHands = true
-SWEP.Primary.Sound = Sound ("weapons/synbf3/dh17_fire.wav");
+SWEP.Primary.Sound = Sound ("weapons/dl44_fire.wav");
 SWEP.Primary.ReloadSound = Sound ("weapons/DC15A_reload.wav");
 SWEP.Primary.KickUp = 2
 SWEP.Weight = 5
 SWEP.AutoSwitchTo = false
 SWEP.AutoSwitchFrom = false
-SWEP.Primary.Recoil = 0.5
-SWEP.Primary.Damage = 110
+SWEP.Primary.Recoil = 0.3
+SWEP.Primary.Damage = 60
 SWEP.Primary.NumShots = 1
 SWEP.Primary.Spread = 0.0125
-SWEP.Primary.IronAccuracy = .001-- Ironsight accuracy, should be the same for shotguns
-SWEP.Primary.ClipSize = 24
+SWEP.Primary.IronAccuracy = .001 -- Ironsight accuracy, should be the same for shotguns
+SWEP.Primary.ClipSize = 15
 SWEP.Primary.RPM = 450
 SWEP.Primary.DefaultClip = 50
 SWEP.Primary.Automatic = true
@@ -46,20 +46,20 @@ SWEP.FireModeName = nil --Change to a text value to override it
 SWEP.Secondary.Automatic	= false
 SWEP.Secondary.Ammo			= "none"
 SWEP.Secondary.IronFOV = 70
---ViewModel 
-SWEP.IronSightsPos = Vector(-4, -0, 0.819)
-SWEP.IronSightsAng = Vector(1, -0, 0)
+SWEP.Primary.Range = -1 -- The distance the bullet can travel in source units.  Set to -1 to autodetect based on damage/rpm.
+SWEP.Primary.RangeFalloff = 0.8 -- The percentage of the range the bullet damage starts to fall off at.  Set to 0.8, for example, to start falling off after 80% of the range.
+SWEP.IronSightsPos = Vector(-4.5, -2, 2.16)
+SWEP.IronSightsAng = Vector(1.7, 2, 0)
 SWEP.ViewModelBoneMods = {
-	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(-0, 0, 0), angle = Angle(0, 0, 0) },
 	["v_dh17_reference001"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
 	["ValveBiped.Bip01_R_Hand"] = { scale = Vector(1, 1, 1), pos = Vector(-0, 0, 0), angle = Angle(0, 0, 0) }
 }
 SWEP.WElements = {
-	["element_name"] = { type = "Model", model = "models/weapons/w_dh17.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(9.869, 0.518, -3.636), angle = Angle(0, -90, 180), size = Vector(0.959, 0.959, 0.959), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["element_name"] = { type = "Model", model = "models/weapons/w_dl44.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(10, 0.4, -4.676), angle = Angle(-10.52, 5.843, 180), size = Vector(0.959, 0.959, 0.959), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 }
 SWEP.VElements = {
-	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_dh17_reference001", rel = "element_reference", pos = Vector(0.4, -10.19, 2.89), angle = Angle(0, 90, 0), size = Vector(0.289, 0.289, 0.289), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
-	["element_reference"] = { type = "Model", model = "models/weapons/w_dh17.mdl", bone = "v_dh17_reference001", rel = "", pos = Vector(0.3, -6, 2.2), angle = Angle(-180, 0, 180), size = Vector(0.82, 0.82, 0.82), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_dh17_reference001", rel = "element_reference", pos = Vector(-8.921, 0.289, 1.639), angle = Angle(0, -180, 0), size = Vector(0.27, 0.27, 0.27), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
+	["element_reference"] = { type = "Model", model = "models/weapons/w_dl44.mdl", bone = "v_dh17_reference001", rel = "", pos = Vector(1, -5, 2.596), angle = Angle(1.169, 87.662, 1.169), size = Vector(0.82, 0.82, 0.82), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 }
 SWEP.BlowbackVector = Vector(0,-3,0.025)
 SWEP.Blowback_Only_Iron  = false
--- tfa_expanded_rework/lua/weapons/tfa_e11_rework/shared.lua
+++ tfa_expanded_rework/lua/weapons/tfa_e11_rework/shared.lua
@@ -2,36 +2,36 @@ if ( SERVER ) then
 	AddCSLuaFile( "shared.lua" )
 end
 if ( CLIENT ) then
-	SWEP.PrintName			= "DH-17"	
-	SWEP.Author				= "TFA, Servius"
-	SWEP.ViewModelFOV      	= 50
-	SWEP.Slot				= 2
-	SWEP.SlotPos			= 3
+	SWEP.PrintName = "E-11"			
+	SWEP.Author = "TFA, Servius"
+	SWEP.ViewModelFOV = 70
+	SWEP.Slot = 2
+	SWEP.SlotPos = 3
 end
 SWEP.Base = "tfa_3dscoped_base"
 SWEP.Category = "TFA Star Wars"
 SWEP.Spawnable = true
 SWEP.AdminSpawnable = true
-SWEP.HoldType = "pistol"
+SWEP.HoldType = "ar2"
 SWEP.ViewModelFOV = 56
 SWEP.ViewModelFlip = false
-SWEP.ViewModel = "models/weapons/synbf3/c_dh17.mdl"
-SWEP.WorldModel = "models/weapons/w_dl44.mdl"
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_irifle.mdl"
 SWEP.ShowViewModel = true
 SWEP.ShowWorldModel = false
 SWEP.UseHands = true
-SWEP.Primary.Sound = Sound ("weapons/synbf3/dh17_fire.wav");
+SWEP.Primary.Sound = Sound ("weapons/synbf3/e11_fire.wav");
 SWEP.Primary.ReloadSound = Sound ("weapons/DC15A_reload.wav");
-SWEP.Primary.KickUp = 2
-SWEP.Weight = 5
+SWEP.Primary.KickUp	= 2
+SWEP.Weight	= 5
 SWEP.AutoSwitchTo = false
-SWEP.AutoSwitchFrom = false
-SWEP.Primary.Recoil = 0.5
-SWEP.Primary.Damage = 110
+SWEP.AutoSwitchFrom	= false
+SWEP.Primary.Recoil	= 0.5
+SWEP.Primary.Damage	= 35
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Spread = 0.0125
-SWEP.Primary.IronAccuracy = .001-- Ironsight accuracy, should be the same for shotguns
-SWEP.Primary.ClipSize = 24
+SWEP.Primary.Spread	= 0.0125
+SWEP.Primary.IronAccuracy = .001	-- Ironsight accuracy, should be the same for shotguns
+SWEP.Primary.ClipSize = 35
 SWEP.Primary.RPM = 450
 SWEP.Primary.DefaultClip = 50
 SWEP.Primary.Automatic = true
@@ -43,23 +43,21 @@ SWEP.OnlyBurstFire = false --No auto, only burst/single?
 SWEP.DefaultFireMode = "" --Default to auto or whatev
 SWEP.FireModeName = nil --Change to a text value to override it
 
-SWEP.Secondary.Automatic	= false
-SWEP.Secondary.Ammo			= "none"
+SWEP.Secondary.Automatic = false
+SWEP.Secondary.Ammo	= "none"
 SWEP.Secondary.IronFOV = 70
---ViewModel 
-SWEP.IronSightsPos = Vector(-4, -0, 0.819)
-SWEP.IronSightsAng = Vector(1, -0, 0)
+--ViewModel
+SWEP.IronSightsPos = Vector(-6.961, -3, 2.16)
+SWEP.IronSightsAng = Vector(0, 0.6, 0)
 SWEP.ViewModelBoneMods = {
-	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(-0, 0, 0), angle = Angle(0, 0, 0) },
-	["v_dh17_reference001"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
-	["ValveBiped.Bip01_R_Hand"] = { scale = Vector(1, 1, 1), pos = Vector(-0, 0, 0), angle = Angle(0, 0, 0) }
+	["v_weapon.awm_parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
 }
 SWEP.WElements = {
-	["element_name"] = { type = "Model", model = "models/weapons/w_dh17.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(9.869, 0.518, -3.636), angle = Angle(0, -90, 180), size = Vector(0.959, 0.959, 0.959), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["element_name"] = { type = "Model", model = "models/weapons/w_e11.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(10, 0.5, -4.7), angle = Angle(0, -90, 170), size = Vector(1.014, 1.014, 1.014), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 }
 SWEP.VElements = {
-	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_dh17_reference001", rel = "element_reference", pos = Vector(0.4, -10.19, 2.89), angle = Angle(0, 90, 0), size = Vector(0.289, 0.289, 0.289), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
-	["element_reference"] = { type = "Model", model = "models/weapons/w_dh17.mdl", bone = "v_dh17_reference001", rel = "", pos = Vector(0.3, -6, 2.2), angle = Angle(-180, 0, 180), size = Vector(0.82, 0.82, 0.82), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_weapon.awm_parent", rel = "element_name", pos = Vector(0.98, -8.829, 2.65), angle = Angle(180, -90, 180), size = Vector(0.3, 0.3, 0.3), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/weapons/w_e11.mdl", bone = "v_weapon.awm_parent", rel = "", pos = Vector(0.899, -4, -6), angle = Angle(-178, 0, -90), size = Vector(1, 1, 1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
 }
 SWEP.BlowbackVector = Vector(0,-3,0.025)
 SWEP.Blowback_Only_Iron  = false
@@ -90,7 +88,7 @@ SWEP.DamageType = DMG_SHOCK
 SWEP.RTMaterialOverride = 0
 SWEP.RTScopeAttachment = -1
 SWEP.Scoped_3D = true
-SWEP.ScopeReticule = "scope/gdcw_green_nobar" 
+SWEP.ScopeReticule = "scope/gdcw_red_nobar" 
 SWEP.Secondary.ScopeZoom = 8
 SWEP.ScopeReticule_Scale = {2.5,2.5}
 SWEP.Secondary.UseACOG			= false	 --Overlay option
