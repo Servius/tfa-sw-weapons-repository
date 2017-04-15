2b9d9a1ce8b1fcb11f0ab33fc450b8757398c857
Servius <nosamrj@gmail.com>
2017-04-15 11:43:51 -0700

    Mega pack scopes finished and moved to scoped base

---
+++ tfa_mega_rework/lua/weapons/tfa_swch_de10_rework/shared.lua
@@ -0,0 +1,91 @@
+if ( SERVER ) then
+	AddCSLuaFile( "shared.lua" )
+end
+if ( CLIENT ) then
+	SWEP.PrintName			= "DE-10"			
+	SWEP.Author				= "Syntax_Error752"
+	SWEP.ViewModelFOV      	= 70
+	SWEP.Slot				= 1
+	SWEP.SlotPos			= 5
+	SWEP.WepSelectIcon = surface.GetTextureID("HUD/killicons/DE10")
+	killicon.Add( "weapon_752_de10", "HUD/killicons/DE10", Color( 255, 80, 0, 255 ) )	
+end
+SWEP.HoldType				= "pistol"
+SWEP.Base					= "tfa_3dscoped_base"
+SWEP.Category = "TFA Star Wars"
+SWEP.Spawnable				= true
+SWEP.AdminSpawnable			= true
+SWEP.ViewModel				= "models/weapons/v_DE10.mdl"
+SWEP.WorldModel				= "models/weapons/w_DE10.mdl"
+SWEP.Primary.Sound = Sound ("weapons/DE10_fire.wav");
+SWEP.Primary.ReloadSound = Sound ("weapons/DE10_reload.wav");
+SWEP.Weight					= 5
+SWEP.AutoSwitchTo			= false
+SWEP.AutoSwitchFrom			= false
+SWEP.Primary.Recoil			= 0.5
+SWEP.Primary.Damage			= 50
+SWEP.Primary.NumShots		= 1
+SWEP.Primary.Spread			= 0.0125
+SWEP.Primary.ClipSize		= 25
+SWEP.Primary.RPM = 60/0.2
+SWEP.Primary.DefaultClip	= 75
+SWEP.Primary.Automatic		= false
+SWEP.Primary.Ammo			= "ar2"
+SWEP.TracerName = "effect_sw_laser_blue"
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo			= "none"
+SWEP.IronSightsPos = Vector(-3.641, -7.974, 1.819)
+SWEP.IronSightsAng = Vector(-1.415, -0.139, 0)
+SWEP.Secondary.IronFOV = 60
+SWEP.VElements = {
+	["element_name"] = { type = "Model", model = "models/rtcircle.mdl", bone = "Box01", rel = "", pos = Vector(0.649, -3.79, -0.08), angle = Angle(0, 180, 90), size = Vector(0.256, 0.256, 0.256), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} }
+}
+SWEP.RTMaterialOverride = -1 --the number of the texture, which you subtract from GetAttachment
+SWEP.RTOpaque = true
+SWEP.DoProceduralReload = true
+SWEP.ProceduralReloadTime = 2
+SWEP.IronSightsSensitivity = 0.25 --Useful for a RT scope.  Change this to 0.25 for 25% sensitivity.  This is if normal FOV compenstaion isn't your thing for whatever reason, so don't change it for normal scopes.
+----Swft Base Code
+SWEP.TracerCount = 1
+SWEP.MuzzleFlashEffect = ""
+SWEP.TracerName = "effect_sw_laser_blue"
+SWEP.Secondary.IronFOV = 70
+SWEP.Primary.KickUp = 0.2
+SWEP.Primary.KickDown = 0.1
+SWEP.Primary.KickHorizontal = 0.1
+SWEP.Primary.KickRight = 0.1
+SWEP.DisableChambering = true
+SWEP.ImpactDecal = "FadingScorch"
+SWEP.ImpactEffect = "effect_sw_impact" --Impact Effect
+SWEP.RunSightsPos = Vector(2.127, 0, 1.355)
+SWEP.RunSightsAng = Vector(-15.775, 10.023, -5.664)
+SWEP.BlowbackEnabled = true
+SWEP.BlowbackVector = Vector(0,-3,0.1)
+SWEP.Blowback_Shell_Enabled = false
+SWEP.Blowback_Shell_Effect = ""
+SWEP.ThirdPersonReloadDisable=false
+SWEP.Primary.DamageType = DMG_SHOCK
+SWEP.DamageType = DMG_SHOCK
+--3dScopedBase stuff
+SWEP.RTMaterialOverride = 0
+SWEP.RTScopeAttachment = -1
+SWEP.Scoped_3D = true
+SWEP.ScopeReticule = "scope/gdcw_blue_nobar" 
+SWEP.Secondary.ScopeZoom = 8
+SWEP.ScopeReticule_Scale = {3,3}
+SWEP.Secondary.UseACOG			= false	 --Overlay option
+SWEP.Secondary.UseMilDot			= false			 --Overlay option
+SWEP.Secondary.UseSVD			= false		 --Overlay option
+SWEP.Secondary.UseParabolic		= false		 --Overlay option
+SWEP.Secondary.UseElcan			= false	 --Overlay option
+SWEP.Secondary.UseGreenDuplex		= false		 --Overlay option
+if surface then
+	SWEP.Secondary.ScopeTable = nil --[[
+		{
+			scopetex = surface.GetTextureID("scope/gdcw_closedsight"),
+			reticletex = surface.GetTextureID("scope/gdcw_acogchevron"),
+			dottex = surface.GetTextureID("scope/gdcw_acogcross")
+		}
+	]]--
+end
+DEFINE_BASECLASS( SWEP.Base )
