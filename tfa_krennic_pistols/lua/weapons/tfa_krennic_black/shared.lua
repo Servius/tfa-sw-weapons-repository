fd7a3d005f958f556c8f566d03c022411f444a4f
Servius <nosamrj@gmail.com>
2017-05-04 01:44:18 -0700

    Krennic Pistols finished

---
+++ tfa_krennic_pistols/lua/weapons/tfa_krennic_black/shared.lua
@@ -0,0 +1,138 @@
+if ( SERVER ) then
+	AddCSLuaFile( "shared.lua" )
+end
+if ( CLIENT ) then
+	SWEP.PrintName = "DT-29"	
+	SWEP.Author = "TFA, Servius"
+	SWEP.ViewModelFOV = 70
+	SWEP.Slot = 2
+	SWEP.SlotPos = 3
+end
+SWEP.Base = "tfa_3dscoped_base"
+SWEP.Category = "TFA Star Wars"
+SWEP.Spawnable = true
+SWEP.AdminSpawnable = true
+SWEP.HoldType = "pistol"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.ViewModel = "models/weapons/synbf3/c_dh17.mdl"
+SWEP.WorldModel = "models/weapons/synbf3/w_dh17.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.UseHands = true
+SWEP.Primary.Sound = Sound ("dt29.single");
+SWEP.Primary.ReloadSound = Sound ("weapons/shared/standard_reload.ogg");
+SWEP.Primary.KickUp = 2
+SWEP.Weight = 5
+SWEP.AutoSwitchTo = false
+SWEP.AutoSwitchFrom = false
+SWEP.Primary.Recoil = 0.5
+SWEP.Primary.Damage = 70
+SWEP.Primary.NumShots = 1
+SWEP.Primary.Spread = 0.0125
+SWEP.Primary.IronAccuracy = .001-- Ironsight accuracy, should be the same for shotguns
+SWEP.Primary.ClipSize = 10
+SWEP.Primary.RPM = 100
+SWEP.Primary.DefaultClip = 50
+SWEP.Primary.Automatic = false
+SWEP.Primary.Ammo = "ar2"
+--Fire Mode
+SWEP.SelectiveFire = false --Allow selecting your firemode?
+SWEP.DisableBurstFire = false --Only auto/single?
+SWEP.OnlyBurstFire = false --No auto, only burst/single?
+SWEP.DefaultFireMode = "" --Default to auto or whatev
+SWEP.FireModeName = nil --Change to a text value to override it
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo			= "none"
+SWEP.Secondary.IronFOV = 70
+--ViewModel 
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_R_Finger42"] = { scale = Vector(1, 1, 1), pos = Vector(0.555, 0, -0.186), angle = Angle(-1.111, 0, 0) },
+	["ValveBiped.Bip01_R_Finger32"] = { scale = Vector(1, 1, 1), pos = Vector(0.925, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger22"] = { scale = Vector(1, 1, 1), pos = Vector(1.667, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger12"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.185, -0.186), angle = Angle(10, 0, 0) },
+	["v_dh17_reference001"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.IronSightsPos = Vector(-5.881, -8.881, 2.72)
+SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.VElements = {
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_dh17_reference001", rel = "element_name", pos = Vector(-3.62, 1.659, 2), angle = Angle(-90, 0, 180), size = Vector(0.22, 0.22, 0.22), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/servius/starwars/battlefront/black_krennic.mdl", bone = "v_dh17_reference001", rel = "", pos = Vector(0, 0, 0), angle = Angle(-90, 90, 0), size = Vector(0.79, 0.79, 0.79), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["element_name"] = { type = "Model", model = "models/servius/starwars/battlefront/black_krennic.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.599, 1.7, -1.5), angle = Angle(97.013, -178, 0), size = Vector(0.699, 0.699, 0.699), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.BlowbackVector = Vector(0,-3,0.025)
+SWEP.Blowback_Only_Iron  = false
+SWEP.DoProceduralReload = true
+SWEP.ProceduralReloadTime = 2.5
+----Swft Base Code
+SWEP.TracerCount = 1
+SWEP.MuzzleFlashEffect = ""
+SWEP.TracerName = "effect_sw_laser_red"
+SWEP.Secondary.IronFOV = 70
+SWEP.Primary.KickUp = 0.2
+SWEP.Primary.KickDown = 0.1
+SWEP.Primary.KickHorizontal = 0.1
+SWEP.Primary.KickRight = 0.1
+SWEP.DisableChambering = true
+SWEP.ImpactDecal = "FadingScorch"
+SWEP.ImpactEffect = "effect_sw_impact" --Impact Effect
+SWEP.RunSightsPos = Vector(2.127, 0, 1.355)
+SWEP.RunSightsAng = Vector(-15.775, 10.023, -5.664)
+SWEP.BlowbackEnabled = true
+SWEP.BlowbackVector = Vector(0,-3,0.1)
+SWEP.Blowback_Shell_Enabled = false
+SWEP.Blowback_Shell_Effect = ""
+SWEP.ThirdPersonReloadDisable=false
+SWEP.Primary.DamageType = DMG_SHOCK
+SWEP.DamageType = DMG_SHOCK
+--3dScopedBase stuff
+SWEP.RTMaterialOverride = 0
+SWEP.RTScopeAttachment = -1
+SWEP.Scoped_3D = true
+SWEP.ScopeReticule = "scope/gdcw_vibrantred_nobar" 
+SWEP.Secondary.ScopeZoom = 10
+SWEP.ScopeReticule_Scale = {2.5,2.5}
+SWEP.Secondary.UseACOG			= false	 --Overlay option
+SWEP.Secondary.UseMilDot			= false			 --Overlay option
+SWEP.Secondary.UseSVD			= false		 --Overlay option
+SWEP.Secondary.UseParabolic		= false		 --Overlay option
+SWEP.Secondary.UseElcan			= false	 --Overlay option
+SWEP.Secondary.UseGreenDuplex		= false		 --Overlay option
+if surface then
+	SWEP.Secondary.ScopeTable = nil --[[
+		{
+			scopetex = surface.GetTextureID("scope/gdcw_closedsight"),
+			reticletex = surface.GetTextureID("scope/gdcw_acogchevron"),
+			dottex = surface.GetTextureID("scope/gdcw_acogcross")
+		}
+	]]--
+end
+DEFINE_BASECLASS( SWEP.Base )
+--[[
+SWEP.HoldType = "pistol"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/synbf3/c_dh17.mdl"
+SWEP.WorldModel = "models/weapons/synbf3/w_dh17.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_R_Finger42"] = { scale = Vector(1, 1, 1), pos = Vector(0.555, 0, -0.186), angle = Angle(-1.111, 0, 0) },
+	["ValveBiped.Bip01_R_Finger32"] = { scale = Vector(1, 1, 1), pos = Vector(0.925, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger22"] = { scale = Vector(1, 1, 1), pos = Vector(1.667, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger12"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.185, -0.186), angle = Angle(10, 0, 0) },
+	["v_dh17_reference001"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.IronSightsPos = Vector(-5.881, -8.881, 2.72)
+SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.VElements = {
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_dh17_reference001", rel = "element_name", pos = Vector(-3.62, 1.659, 2), angle = Angle(-90, 0, 0), size = Vector(0.2, 0.2, 0.2), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/servius/starwars/battlefront/black_krennic.mdl", bone = "v_dh17_reference001", rel = "", pos = Vector(0, 0, 0), angle = Angle(-90, 90, 0), size = Vector(0.79, 0.79, 0.79), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["element_name"] = { type = "Model", model = "models/servius/starwars/battlefront/black_krennic.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(3.599, 1.7, -1.5), angle = Angle(97.013, -178, 0), size = Vector(0.699, 0.699, 0.699), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+]]
