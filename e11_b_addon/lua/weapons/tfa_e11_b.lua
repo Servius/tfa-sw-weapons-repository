da204c45452372627d8e533aa4e5e95bf21f93b1
Servius <nosamrj@gmail.com>
2017-05-18 11:20:24 -0700

    Two new addons local to live;Sound modification

---
+++ e11_b_addon/lua/weapons/tfa_e11_b.lua
@@ -0,0 +1,143 @@
+if ( SERVER ) then
+	AddCSLuaFile( "shared.lua" )
+end
+if ( CLIENT ) then
+	SWEP.PrintName			= "E-11B"			
+	SWEP.Author				= "TFA, Servius, Venator"
+	SWEP.ViewModelFOV      	= 50
+	SWEP.Slot				= 2
+	SWEP.SlotPos			= 3
+end
+SWEP.Base					= "tfa_3dscoped_base"
+SWEP.Category = "TFA Star Wars"
+SWEP.Spawnable				= true
+SWEP.AdminSpawnable			= true
+SWEP.HoldType = "ar2"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_dc15sa.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.UseHands = true
+SWEP.Primary.Sound = Sound ("weapons/e11/e11_fire.ogg");
+SWEP.Primary.ReloadSound = Sound ("weapons/shared/battlefront_standard_reload.ogg");
+SWEP.Primary.KickUp			= 2
+SWEP.Weight					= 5
+SWEP.AutoSwitchTo			= false
+SWEP.AutoSwitchFrom			= false
+SWEP.Primary.Recoil			= 0.4
+SWEP.Primary.Damage			= 44
+SWEP.Primary.NumShots		= 1
+SWEP.Primary.Spread			= 0.0125
+SWEP.Primary.IronAccuracy = .001	-- Ironsight accuracy, should be the same for shotguns
+SWEP.Primary.ClipSize		= 45
+SWEP.Primary.RPM            = 325
+SWEP.Primary.DefaultClip	= 50
+SWEP.Primary.Automatic		= true
+SWEP.Primary.Ammo			= "ar2"
+SWEP.SelectiveFire		= true --Allow selecting your firemode?
+SWEP.DisableBurstFire	= false --Only auto/single?
+SWEP.OnlyBurstFire		= false --No auto, only burst/single?
+SWEP.DefaultFireMode 	= "" --Default to auto or whatev
+SWEP.FireModes = {
+"Auto",
+"3Burst",
+"Single"
+}
+SWEP.FireModeName = nil --Change to a text value to override it
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo			= "none"
+SWEP.Secondary.IronFOV = 70
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(0.924, 0.924, 0.924), pos = Vector(-2.037, -0.556, -0.926), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger1"] = { scale = Vector(1, 1, 1), pos = Vector(1.296, -0.186, 0.185), angle = Angle(0, 0, 0) },
+	["v_weapon.awm_parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger2"] = { scale = Vector(1, 1, 1), pos = Vector(-0.556, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger3"] = { scale = Vector(1, 1, 1), pos = Vector(-0.556, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger4"] = { scale = Vector(1, 1, 1), pos = Vector(-0.926, -0.186, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.IronSightsPos = Vector(-7.441, -4.422, 2.119)
+SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.VElements = {
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_weapon.awm_parent", rel = "element_name", pos = Vector(-3.5, 0, 12.199), angle = Angle(180, 0, 180), size = Vector(0.419, 0.419, 0.419), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/servius/starwars/venator_custom/e11_b.mdl", bone = "v_weapon.awm_parent", rel = "", pos = Vector(0, 5.5, -1.558), angle = Angle(-90, 90, 0), size = Vector(0.899, 0.899, 0.899), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["element_name"] = { type = "Model", model = "models/servius/starwars/venator_custom/e11_b.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(4.675, 1.299, 5.714), angle = Angle(-10.52, -2, 180), size = Vector(0.899, 0.899, 0.899), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.BlowbackVector = Vector(0,-3,0.025)
+SWEP.Blowback_Only_Iron  = false
+SWEP.DoProceduralReload = true
+SWEP.ProceduralReloadTime = 2.5
+----Swft Base Code
+SWEP.TracerCount = 1
+SWEP.MuzzleFlashEffect = ""
+SWEP.TracerName = "effect_sw_laser_red"
+SWEP.Secondary.IronFOV = 70
+SWEP.Primary.KickUp = 0.2
+SWEP.Primary.KickDown = 0.1
+SWEP.Primary.KickHorizontal = 0.1
+SWEP.Primary.KickRight = 0.1
+SWEP.DisableChambering = true
+SWEP.ImpactDecal = "FadingScorch"
+SWEP.ImpactEffect = "effect_sw_impact" --Impact Effect
+SWEP.RunSightsPos = Vector(2.127, 0, 1.355)
+SWEP.RunSightsAng = Vector(-15.775, 10.023, -5.664)
+SWEP.BlowbackEnabled = true
+SWEP.BlowbackVector = Vector(0,-3,0.1)
+SWEP.Blowback_Shell_Enabled = false
+SWEP.Blowback_Shell_Effect = ""
+SWEP.ThirdPersonReloadDisable=false
+SWEP.Primary.DamageType = DMG_SHOCK
+SWEP.DamageType = DMG_SHOCK
+--3dScopedBase stuff
+SWEP.RTMaterialOverride = 0
+SWEP.RTScopeAttachment = -1
+SWEP.Scoped_3D = true
+SWEP.ScopeReticule = "scope/gdcw_vibrantred_nobar" 
+SWEP.Secondary.ScopeZoom = 8
+SWEP.ScopeReticule_Scale = {2.5,2.5}
+SWEP.Secondary.UseACOG			= false	 --Overlay option
+SWEP.Secondary.UseMilDot			= false			 --Overlay option
+SWEP.Secondary.UseSVD			= false		 --Overlay option
+SWEP.Secondary.UseParabolic		= false		 --Overlay option
+SWEP.Secondary.UseElcan			= false	 --Overlay option
+SWEP.Secondary.UseGreenDuplex		= false		 --Overlay option
+if surface then
+	SWEP.Secondary.ScopeTable = nil --[[
+		{
+			scopetex = surface.GetTextureID("scope/gdcw_closedsight"),
+			reticletex = surface.GetTextureID("scope/gdcw_acogchevron"),
+			dottex = surface.GetTextureID("scope/gdcw_acogcross")
+		}
+	]]--
+end
+DEFINE_BASECLASS( SWEP.Base )
+--[[
+SWEP.HoldType = "ar2"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_dc15sa.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(0.924, 0.924, 0.924), pos = Vector(-2.037, -0.556, -0.926), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger1"] = { scale = Vector(1, 1, 1), pos = Vector(1.296, -0.186, 0.185), angle = Angle(0, 0, 0) },
+	["v_weapon.awm_parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger2"] = { scale = Vector(1, 1, 1), pos = Vector(-0.556, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger3"] = { scale = Vector(1, 1, 1), pos = Vector(-0.556, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger4"] = { scale = Vector(1, 1, 1), pos = Vector(-0.926, -0.186, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.IronSightsPos = Vector(-7.441, -4.422, 2.119)
+SWEP.IronSightsAng = Vector(0, 0, 0)
+SWEP.VElements = {
+	["element_scope"] = { type = "Model", model = "models/rtcircle.mdl", bone = "v_weapon.awm_parent", rel = "element_name", pos = Vector(-3.5, 0, 12.199), angle = Angle(180, 0, 0), size = Vector(0.419, 0.419, 0.419), color = Color(255, 255, 255, 255), surpresslightning = false, material = "!tfa_rtmaterial", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/servius/starwars/venator_custom/e11_b.mdl", bone = "v_weapon.awm_parent", rel = "", pos = Vector(0, 5.5, -1.558), angle = Angle(-90, 90, 0), size = Vector(0.899, 0.899, 0.899), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["element_name"] = { type = "Model", model = "models/servius/starwars/venator_custom/e11_b.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(4.675, 1.299, 5.714), angle = Angle(-10.52, -2, 180), size = Vector(0.899, 0.899, 0.899), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+--]]
