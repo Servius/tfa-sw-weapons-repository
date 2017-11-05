1f67403ba8c46f8c4d44a81102aab39677ce671d
Servius <nosamrj@gmail.com>
2017-11-05 12:04:40 -0800

    live addons organized by addon uploader

---
+++ Live Addons/servius_uploader/tfa_swrp_expanded_weapons_pack/lua/weapons/chaingun_servius/shared.lua
@@ -0,0 +1,107 @@
+if ( SERVER ) then
+	AddCSLuaFile( "shared.lua" )
+end
+if ( CLIENT ) then
+	SWEP.PrintName			= "Clone Minigun"			
+	SWEP.Author				= "TFA, Servius"
+	SWEP.ViewModelFOV      	= 90.251256281407
+	SWEP.Slot				= 2
+	SWEP.SlotPos			= 3
+end
+SWEP.HoldType				= "rpg"
+SWEP.Base					= "tfa_swsft_base_servius"
+SWEP.Category = "TFA Star Wars"
+SWEP.Spawnable				= true
+SWEP.AdminSpawnable			= true
+SWEP.ViewModelFOV = 90.251256281407
+SWEP.ViewModelFlip = false
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_irifle.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = false
+SWEP.UseHands = true
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_L_Finger31"] = { scale = Vector(1, 1, 1), pos = Vector(0, -0.556, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(2, -1.5, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger42"] = { scale = Vector(1, 1, 1), pos = Vector(-0.186, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger2"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.4, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger41"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(-5.557, 0, 0) },
+	["ValveBiped.Bip01_R_Finger1"] = { scale = Vector(1, 1, 1), pos = Vector(-1, 0, 0.4), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger4"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.185, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger3"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.36, 0), angle = Angle(0, 0, 0) },
+	["v_weapon.awm_parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger32"] = { scale = Vector(1, 1, 1), pos = Vector(-0.186, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_UpperArm"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(-30, -4.259, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.Primary.Sound = Sound ("weapons/z6_rotary/repeat-1.wav");
+SWEP.Primary.ReloadSound = Sound ("weapons/shared/standard_reload.ogg");
+SWEP.Primary.KickUp			= 0.5
+SWEP.Weight					= 15
+SWEP.AutoSwitchTo			= false
+SWEP.AutoSwitchFrom			= false
+SWEP.Primary.Recoil			= 0.5
+SWEP.Primary.Damage			= 30
+SWEP.Primary.NumShots		= 1
+SWEP.Primary.Spread			= 0.005
+SWEP.Primary.IronAccuracy = .005	-- Ironsight accuracy, should be the same for shotguns
+SWEP.Primary.ClipSize		= 400
+SWEP.Primary.RPM            = 535
+SWEP.Primary.DefaultClip	= 800
+SWEP.Primary.Automatic		= true
+SWEP.Primary.Ammo			= "ar2"
+SWEP.TracerName = "effect_sw_laser_blue"
+SWEP.SelectiveFire		= true --Allow selecting your firemode?
+SWEP.DisableBurstFire	= false --Only auto/single?
+SWEP.OnlyBurstFire		= false --No auto, only burst/single?
+SWEP.DefaultFireMode 	= "" --Default to auto or whatev
+SWEP.FireModes = {
+"Auto",
+"12Burst"
+}
+SWEP.FireModeName = nil --Change to a text value to override it
+SWEP.Secondary.Automatic	= false
+SWEP.Secondary.Ammo			= "none"
+SWEP.Secondary.IronFOV = 70
+SWEP.IronSightsPos = Vector(-0, 0, 0)
+SWEP.IronSightsAng = Vector(-0, 0, 0)
+SWEP.VElements = {
+	["element_name"] = { type = "Model", model = "models/swbf3/rep/republicchaingun.mdl", bone = "v_weapon.awm_parent", rel = "", pos = Vector(0, 12, 18.5), angle = Angle(-90, 90, 0), size = Vector(0.949, 0.949, 0.949), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["models/swbf3/imp/minigun.mdl"] = { type = "Model", model = "models/swbf3/rep/republicchaingun.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(-8.3, 1.557, 10), angle = Angle(-7, 0, -180), size = Vector(0.85, 0.85, 0.85), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/bf2/rep_weap_inf_chaingun", skin = 0, bodygroup = {} }
+}
+SWEP.BlowbackVector = Vector(0,-3,0.025)
+SWEP.Blowback_Only_Iron  = false
+SWEP.DoProceduralReload = true
+SWEP.ProceduralReloadTime = 2.5
+
+SWEP.MoveSpeed = 0.7
+
+--[[
+SWEP.HoldType = "rpg"
+SWEP.ViewModelFOV = 90.251256281407
+SWEP.ViewModelFlip = false
+SWEP.UseHands = true
+SWEP.ViewModel = "models/weapons/cstrike/c_snip_awp.mdl"
+SWEP.WorldModel = "models/weapons/w_irifle.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = true
+SWEP.ViewModelBoneMods = {
+	["ValveBiped.Bip01_L_Finger31"] = { scale = Vector(1, 1, 1), pos = Vector(0, -0.556, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_UpperArm"] = { scale = Vector(1, 1, 1), pos = Vector(2, -1.5, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger42"] = { scale = Vector(1, 1, 1), pos = Vector(-0.186, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger2"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.4, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger41"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0, 0), angle = Angle(-5.557, 0, 0) },
+	["ValveBiped.Bip01_R_Finger1"] = { scale = Vector(1, 1, 1), pos = Vector(-1, 0, 0.4), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger4"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.185, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_R_Finger3"] = { scale = Vector(1, 1, 1), pos = Vector(0, 0.36, 0), angle = Angle(0, 0, 0) },
+	["v_weapon.awm_parent"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(0, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_Finger32"] = { scale = Vector(1, 1, 1), pos = Vector(-0.186, 0, 0), angle = Angle(0, 0, 0) },
+	["ValveBiped.Bip01_L_UpperArm"] = { scale = Vector(0.009, 0.009, 0.009), pos = Vector(-30, -4.259, 0), angle = Angle(0, 0, 0) }
+}
+SWEP.VElements = {
+	["element_name"] = { type = "Model", model = "models/swbf3/rep/republicchaingun.mdl", bone = "v_weapon.awm_parent", rel = "", pos = Vector(0, 12, 18.5), angle = Angle(-90, 90, 0), size = Vector(0.949, 0.949, 0.949), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["models/swbf3/imp/minigun.mdl"] = { type = "Model", model = "models/swbf3/rep/republicchaingun.mdl", bone = "ValveBiped.Bip01_R_Hand", rel = "", pos = Vector(-8.3, 1.557, 10), angle = Angle(-7, 0, -180), size = Vector(0.85, 0.85, 0.85), color = Color(255, 255, 255, 255), surpresslightning = false, material = "models/bf2/rep_weap_inf_chaingun", skin = 0, bodygroup = {} }
+}--]]
